1d7e5292046b0706861dda7a6937b2b191e6285d
D. Byrchenko <byrchenko@gmail.com>
2020-02-05 18:02:29 +0200

    assessment questions 2

---
+++ src/AssessmentQuestions/Next.jsx
@@ -0,0 +1,18 @@
+import React from "react";
+import PropTypes from "prop-types";
+import css from "./Next.scss";
+
+const Next = props => {
+
+	return (
+		<div className={css.index}>
+			{"Next"}
+		</div>
+	);
+};
+
+Next.propTypes = {};
+
+Next.defaultProps = {};
+
+export default Next;
---
+++ src/AssessmentQuestions/Previous.jsx
@@ -0,0 +1,18 @@
+import React from "react";
+import PropTypes from "prop-types";
+import css from "./Previous.scss";
+
+const Previous = props => {
+
+	return (
+		<div className={css.index}>
+			{"Previous"}
+		</div>
+	);
+};
+
+Previous.propTypes = {};
+
+Previous.defaultProps = {};
+
+export default Previous;
